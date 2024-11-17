a2cb57d153060b59da19f3cd139855a9e5740e23
pyeojim <pyeojim@gmail.com>
2024-11-17 20:07:58 +0900

    d

--- C/test.cpp
+++ C/test.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int iNum = 0;
-int iNums[3] = { 1, 2, 3 };
-int* pNum = &iNum;
-int* pNums1 = &iNums[0];
-int* pNums2 = iNums; // &iNums ?
-cout << pNums1 << endl << pNums2 << endl;
-cout << *pNums1 << endl << *pNums2 << endl;
-cout << iNums[1] << endl << pNums1[1] << endl << pNums2[1] << endl;
+int main() {
+	int iNum = 0;
+	int iNums[3] = { 1, 2, 3 };
+	int* pNum = &iNum;
+	int* pNums1 = &iNums[0];
+	int* pNums2 = iNums; // &iNums ?
+	cout << pNums1 << endl << pNums2 << endl;
+	cout << *pNums1 << endl << *pNums2 << endl;
+	cout << iNums[1] << endl << pNums1[1] << endl << pNums2[1] << endl;
+	return 0;
+}

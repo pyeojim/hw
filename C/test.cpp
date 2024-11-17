32f5f5a656b809c3f641eb794f74be5712342c1d
pyeojim <pyeojim@gmail.com>
2024-11-17 19:38:52 +0900

    d

---
+++ C/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int iNum = 0;
+int iNums[3] = { 1, 2, 3 };
+int* pNum = &iNum;
+int* pNums1 = &iNums[0];
+int* pNums2 = iNums; // &iNums ?
+cout << pNums1 << endl << pNums2 << endl;
+cout << *pNums1 << endl << *pNums2 << endl;
+cout << iNums[1] << endl << pNums1[1] << endl << pNums2[1] << endl;
